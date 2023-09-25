bdc4767bc90e6a5d9a1bf5c3ad320a50e76db001
Nicholas Ali <etw.business@gmail.com>
2023-09-25 14:31:20 -0400

    ss

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from api.views import RoomView
+from api.views import RoomView, CreateRoomView, GetRoom
 
 urlpatterns = [
-    path("room", RoomView.as_view(), name="room-create"),  
+    path("room", RoomView.as_view(), name="room-create"),
+    path("create-room", CreateRoomView.as_view(), name="create-room"),
+    path("get-room", RoomView.as_view(), name="get-room"),
 ]
